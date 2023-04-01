27f0f038fedeabc18f1d9f4f9f81e2b440729574
SPanchai <129434556+SPanchai@users.noreply.github.com>
2023-04-01 10:29:26 +0700

    Update order.py

--- order.py
+++ order.py
@@ -11,36 +11,40 @@ import os
 class TurtleBot:
     def __init__(self):
         rospy.init_node('turtlebot_subscriber')
+	
+	#sending a speed value to bot
         self.velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.cmd_vel = Twist()
         
+	#getting an word from topic
         rospy.Subscriber('chatter', String, self.words_callback)
         
         
-    def words_callback(self, data):
-        # Check if the word is "run"
+    def words_callback(self, data):       
         
+	#Reset speed, force to stop
         self.cmd_vel.linear.x = 0
+	
+	#getting an word from topic
         mytext = data.data       
 
         if mytext == 'order':
-            # Set the linear speed of the robot to maximum
-            self.cmd_vel.linear.x = 0.2  # linear speed is 0.2 m/s
-            # Set the angular speed of the robot to zero
-            self.cmd_vel.angular.z = 0
+            # check if user call for ordering menu
+	
+            self.cmd_vel.linear.x = 0.2
+            self.cmd_vel.angular.z = 0	
             mytext = "What's order"
-            
-            
-            
-            
+            # setting speed and word for condition 1 go to table       
+                                    
         elif mytext != 'order':
-            # Set the linear speed of the robot to maximum
-            self.cmd_vel.linear.x = -0.2  # linear speed is 0.2 m/s
-            # Set the angular speed of the robot to zero
-            self.cmd_vel.angular.z = 0            
+            # check what user order
+            self.cmd_vel.linear.x = -0.2
+            self.cmd_vel.angular.z = 0 
+            # setting speed and word for condition 2 go back to cashier     
         language = 'en'
         myobj = gTTS(text=mytext, lang=language, slow=False)
-        myobj.save("welcome.mp3")      
+        myobj.save("welcome.mp3")
+	#save text to sound
         
            
             
@@ -48,19 +52,22 @@ class TurtleBot:
 
     def run(self):
     	while not rospy.is_shutdown():
+		
 	    	start_time = rospy.Time.now()
-	    	cv = self.velocity_publisher.publish(self.cmd_vel)   	
+	    	cv = self.velocity_publisher.publish(self.cmd_vel) 	
 	   	
-	    	if cv != 0:	    	   	
+	    	if cv != 0:
+		#check currents velocity, checking bot is moving	    	   	
 	    		while (rospy.Time.now() - start_time).to_sec() < 5.0:
 	    			self.velocity_publisher.publish(self.cmd_vel)
+			#sending speed to move for 5 seconds
+			
 	    		self.cmd_vel.linear.x = 0
 	    		self.velocity_publisher.publish(self.cmd_vel)
-	    		
+	    		#stop bot
 	    	os.system("mpg123 welcome.mp3")
+		#play last getting sound   		
 	    		
-	    		
-
 
 
 if __name__ == '__main__':
